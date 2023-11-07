984b44cfb0c3b86b625817c593b71b54816e7888
Jong <jm991014@gmail.com>
2023-11-08 00:24:15 +0900

    [add] 메인 + 로그인 레이아웃 추가

--- app/src/main/java/com/example/umc_velog_aos/MainActivity.kt
+++ app/src/main/java/com/example/umc_velog_aos/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.umc_velog_aos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.umc_velog_aos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btnMainLogin.setOnClickListener {
+
+        }
     }
 }
