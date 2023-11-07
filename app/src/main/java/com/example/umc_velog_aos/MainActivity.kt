8959a97d1ec00df4ec75b556cace2b2a18e7cff8
Jong <jm991014@gmail.com>
2023-11-07 16:37:26 +0900

    initial commit

---
+++ app/src/main/java/com/example/umc_velog_aos/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.umc_velog_aos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
