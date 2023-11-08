450c37b85a69024c6ae9c8b812f8eef795d22342
Jong <jm991014@gmail.com>
2023-11-08 23:10:02 +0900

    [add] signup 페이지 레이아웃 구현

--- app/src/main/java/com/example/umc_velog_aos/SignupFragment.kt
+++ app/src/main/java/com/example/umc_velog_aos/SignupFragment.kt
@@ -1,9 +1,17 @@
 package com.example.umc_velog_aos
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.umc_velog_aos.databinding.FragmentSignupBinding
 
@@ -16,6 +24,15 @@ class SignupFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSignupBinding.inflate(inflater, container, false)
+
+        val textData: String = binding.tvSignupPolicy.text.toString()
+        val builder = SpannableStringBuilder(textData)
+        val colorSpan = ForegroundColorSpan(Color.parseColor("#12b886"))
+        builder.setSpan(colorSpan, 0, 3, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        builder.setSpan(colorSpan, 6, 13, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        binding.tvSignupPolicy.text = builder
+
 
         return binding.root
     }
